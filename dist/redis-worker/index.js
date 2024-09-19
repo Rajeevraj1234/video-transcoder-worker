7510550d95720c4df3f2d633c4507e5ad17011ac
Rajeevraj1234 <rarajeevraj@gmail.com>
2024-09-19 20:51:07 +0530

    fix/ docker always downalos a 139mb file

--- dist/redis-worker/index.js
+++ dist/redis-worker/index.js
@@ -12,30 +12,45 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-//import { db } from "../db";
 const worker_1 = __importDefault(require("../worker"));
+const axios_1 = __importDefault(require("axios"));
 const index_1 = __importDefault(require("../redis/index")); // Set to "nextnode" if you encounter module resolution errors
 const client = index_1.default.getInstance();
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const QUEUE_NAME = "transcodingData"; // Define your queue name here
 const POLL_INTERVAL_MS = 1000; // Polling interval
 function addMoveToDbFromRedis() {
     return __awaiter(this, void 0, void 0, function* () {
         while (true) {
+            let parsedData;
             try {
                 // Use BRPOP to wait for an element to be available
                 const data = yield client.brPop(QUEUE_NAME, 0); // 0 means to block indefinitely (block until new element comes in)
-                const parsedData = JSON.parse((data === null || data === void 0 ? void 0 : data.element) || "");
-                console.log("parsed data is here", parsedData);
+                parsedData = JSON.parse((data === null || data === void 0 ? void 0 : data.element) || "");
                 if (data) {
                     yield (0, worker_1.default)(parsedData);
                 }
                 else {
-                    console.log("Unexpected error: No data returned");
+                    yield axios_1.default.post(`${process.env.FRONTEND_URL}`, {
+                        message: "Data is not present",
+                        success: false,
+                    });
                 }
             }
             catch (error) {
-                console.error("Error occurred in Game/addMoveToDb", error);
+                yield axios_1.default.post(`${process.env.FRONTEND_URL}`, {
+                    message: "Video did not uploaded successfully",
+                    success: false,
+                });
+                console.error("Error occured in RER", error);
             }
+            //sen message that process has been completed
+            yield axios_1.default.post(`${process.env.FRONTEND_URL}`, {
+                message: "Video upload successfully",
+                success: true,
+                data: parsedData,
+            });
             // Add a delay before the next BRPOP call, if needed
             // This helps to avoid excessive CPU usage in case of errors or other issues
             yield new Promise((resolve) => setTimeout(resolve, POLL_INTERVAL_MS));
--- src/redis-worker/index.ts
+++ src/redis-worker/index.ts
@@ -1,26 +1,41 @@
-//import { db } from "../db";
 import initTranscoding from "../worker";
+import axios from "axios";
 import RedisClient from "../redis/index"; // Set to "nextnode" if you encounter module resolution errors
 const client = RedisClient.getInstance();
+import dotenv from "dotenv";
+dotenv.config();
 
 const QUEUE_NAME = "transcodingData"; // Define your queue name here
 const POLL_INTERVAL_MS = 1000; // Polling interval
 
 async function addMoveToDbFromRedis() {
   while (true) {
+    let parsedData;
     try {
       // Use BRPOP to wait for an element to be available
       const data = await client.brPop(QUEUE_NAME, 0); // 0 means to block indefinitely (block until new element comes in)
-      const parsedData = JSON.parse(data?.element || "");
-      console.log("parsed data is here", parsedData);
+      parsedData = JSON.parse(data?.element || "");
       if (data) {
         await initTranscoding(parsedData);
       } else {
-        console.log("Unexpected error: No data returned");
+        await axios.post(`${process.env.FRONTEND_URL}`, {
+          message: "Data is not present",
+          success: false,
+        });
       }
     } catch (error) {
-      console.error("Error occurred in Game/addMoveToDb", error);
+      await axios.post(`${process.env.FRONTEND_URL}`, {
+        message: "Video did not uploaded successfully",
+        success: false,
+      });
+      console.error("Error occured in RER", error);
     }
+    //sen message that process has been completed
+    await axios.post(`${process.env.FRONTEND_URL}`, {
+      message: "Video upload successfully",
+      success: true,
+      data: parsedData,
+    });
 
     // Add a delay before the next BRPOP call, if needed
     // This helps to avoid excessive CPU usage in case of errors or other issues
